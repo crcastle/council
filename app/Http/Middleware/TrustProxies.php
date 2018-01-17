bf0216ea4b1c4cdb58a5449ad7fca42514828d5b
Chris Castle <castle@heroku.com>
2018-01-16 19:55:40 -0800

    Trust Heroku proxy servers

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Request;
+use Fideloper\Proxy\TrustProxies as Middleware;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * @var array
+     */
+    protected $proxies = '**';
+
+    /**
+     * The current proxy header mappings.
+     *
+     * @var array
+     */
+    protected $headers = [
+        Request::HEADER_FORWARDED    => null, // not set on AWS or Heroku
+        Request::HEADER_CLIENT_IP    => 'X_FORWARDED_FOR',
+        Request::HEADER_CLIENT_HOST  => null, // not set on AWS or Heroku
+        Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_PROTO',
+        Request::HEADER_CLIENT_PORT  => 'X_FORWARDED_PORT',
+    ];
+}
